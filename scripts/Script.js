931affbb7974cf14d8cc91f2bea899f5807592d7
Maxim Chizhevskiy <max15.91@mail.ru>
2024-02-27 00:05:51 +0600

    Large map & Limits of Clicks

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,6 +1,7 @@
-var width = 400
-var height = 400
+var width = 800
+var height = 800
 var clicks = 0
+var limitsOfClick = 20
 
 var getRandomNumber = function (size) {
     return Math.floor(Math.random() * size)
@@ -18,18 +19,22 @@ var getDistance = function (event, target) {
 }
 
 var getDistanceHint = function (distance) {
-    if (distance < 10) {
+    if (distance < 20) {
         return "Обожжешься!";
-    } else if (distance < 20) {
-        return "Очень горячо";
+    } else if (distance < 30) {
+        return "Очень-очень горячо";
     } else if (distance < 40) {
-        return "Горячо";
+        return "Очень горячо";
     } else if (distance < 80) {
-        return "Тепло";
+        return "Горячо";
     } else if (distance < 160) {
-        return "Холодно";
+        return "Тепло";
     } else if (distance < 320) {
+        return "Холодно";
+    } else if (distance < 480)
         return "Очень холодно";
+    else if (distance < 640) {
+        return "Очень-очень холодно";
     } else {
         return "Замерзнешь!";
     }
@@ -37,11 +42,20 @@ var getDistanceHint = function (distance) {
 
 $("#map").click(function (event) {
     clicks++
+    limitsOfClick--
     var distance = getDistance(event,target)
     var distanceHint = getDistanceHint(distance)
     $("#distance").text(distanceHint)
+    $("#clicksLeft").text("Осталось кликов " + limitsOfClick)
 
-    if (distance < 8) {
+    if (limitsOfClick <= 0) {
+        $("#heading").text("Клад, не найден")
+        $("#clicksLeft").text("Осталось кликов 0")
+        $("#distance").text('')
+    }
+
+    if (distance < 16) {
+        $("#heading").text("Клад найден!")
         alert("Клад найден! Сделано кликов " + clicks)
     }
 })
